6787fc3e6fba8ee0a41c1d55be965bc3cf71d124
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2024-10-04 09:13:08 +0700

    modipp

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,15 @@ function App() {
 
   return (
     <>
-      <h1>First React App</h1>
-      <p>This is the first React App</p>
+      <section className='main'>
+        <h1>First React App</h1>
+        <p>This is the first React App</p>
 
-      <div className="button-wrap flex-row">
-        <button onClick={() => setCount((count) => count + 1)}>Test Count {count == 0? "" : count}</button>
-        <button>Other Page</button>
-      </div>
+        <div className="button-wrap flex-row">
+          <button className='btn btn-primary' onClick={() => setCount((count) => count + 1)}>Test Count {count == 0 ? "" : count}</button>
+          <button className='btn btn-secondary'>Second Btn</button>
+        </div>
+      </section>
     </>
   )
 }
